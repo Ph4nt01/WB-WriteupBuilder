26ae28205dafa07f3cb6d269f5668594c5f03b4b
Ph4nt01 <sorush986@gmail.com>
2025-08-23 17:38:32 +0330

    New Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wb-writeupbuilder",
-    version="1.0.4",
+    version="1.0.5",
     description="Builds Writup with your input.",
     author="[Ph4nt01]",
     author_email="ph4nt0.84@gmail.com",
